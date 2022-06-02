0cdfcd019dba4de4d32ed10d7783e12d0a10fbd0
Mark Gerken <mgerken86@gmail.com>
2022-06-02 16:47:39 -0600

    More creation of folders and files

--- server.js
+++ server.js
@@ -5,7 +5,28 @@ const port = 3000
 app.listen(port, () => {
     console.log('Listening on Budget Deliverable')
 })
+app.use((req, res, next) => {
+    console.log('I run for all routes')
+    next()
+})
+app.use(express.urlencoded( { extended: false }))
 
-app.get('/', (req, res) => {
-    res.send('test')
+app.post('/budgets', (req, res) => {
+    res.redirect('/budget')
+})
+
+app.get('/budgets', (req, res) => {
+    res.render('index', {
+
+    })
+})
+
+app.get('/budgets/new', (req, res) => {
+    
+})
+
+app.get('/budgets/:index', (req, res) => {
+    res.render('show', {
+        
+    })
 })
