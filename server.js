7fa0d1fef3962e651df0905cfe79a6d776937d89
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-02 17:50:19 -0400

    Index route displaying budget list on a table using skeleton

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const express = require('express');
 const app = require('liquid-express-views')(express());
 const port = 3000;
 const methodOverride = require('method-override');
+const Budget = require('./models/budget')
 
 //! Middlewares
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
 
-app.get('/budgets', (req, res) => {});
+//? Index route
+app.get('/budgets', (req, res) => {
+  res.render('index', {Budget});
+});
 
+//? Show route
 app.get('/budgets/:index', (req, res) => {});
 
+//? New route
 app.get('/budgets/new', (req, res) => {});
 
+//? Create route
 app.post('/budgets', (req, res) => {});
 
 app.listen(port, () => {
