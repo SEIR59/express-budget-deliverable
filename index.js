4f2ecb5632fc875bfca464f02b2d7a6de6879377
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-06-02 17:29:55 -0400

    show page rendering, button brings back to index

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = require('liquid-express-views')(express())
 const port = 3000;
 
-const budgetItems = require('./models/budget.js')
 
 app.listen(port, () =>{
     console.log('listening on port:', port)
@@ -14,4 +13,10 @@ app.listen(port, () =>{
 
 app.get('/', (req,res) =>{
    res.render('index',{allBudgetItems: budgetItems})
+})
+
+app.get('/:indexOfBudgetItems', (req,res) =>{
+    res.render('show',{
+        item:budgetItems[req.params.indexOfBudgetItems]
+    })
 })
