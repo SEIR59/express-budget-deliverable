55d65ab73462a5e4193b7ed1718272f0c95338f9
andersons1218 <andersons1218@gmail.com>
2022-06-02 19:07:50 -0400

    getting started

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = require("liquid-express-views")(express())
+const budget = require('./models/budget.js')
+const port = 3000
+
+app.use(express.static('public'))
+//near the top, around other app.use() calls
+app.use(express.urlencoded({extended:false}));
+
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+
+app.listen(port, () =>{
+    console.log("listen to port 3000")
+})
+app.get('/' , (req, res) =>{
+    res.send('hello world')
+})
+app.get('/budgets', (req, res) =>{
+    res.render('index')
+})
+
+app.get('/budgets/:index', (req, res) =>{
+    res.render ('show',
+    {budget: budget[req.params.id]})
+})
