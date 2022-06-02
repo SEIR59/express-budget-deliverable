8f0ea9af388d3df89d3fcdf05ce081fd75806265
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-02 17:33:26 -0400

    Initial setup for all the routes needed

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const app = require("liquid-express-views")(express());
+const app = require('liquid-express-views')(express());
 const port = 3000;
 const methodOverride = require('method-override');
-
 
 //! Middlewares
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
 
+app.get('/budgets', (req, res) => {});
 
+app.get('/budgets/:index', (req, res) => {});
+
+app.get('/budgets/new', (req, res) => {});
+
+app.post('/budgets', (req, res) => {});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
