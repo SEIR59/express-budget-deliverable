51a45deb628be1edabe9489a062fe9ddd008fcef
patrickbasham <PatrickTBasham@gmail.com>
2022-06-02 17:32:39 -0400

    COMPLETE: updated format of dates to only display month and day, finished routes

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ const port = 3000
 app.use(expr.urlencoded({extended: false}))
 // sets public as the default file to search in for files (ex: .css/.js)
 app.use(expr.static("public"))
-
+// get array for 'database'
 const arrBudget = require("./models/budget.js")
+// set bank account variable
+let bankAccount = 0;
+
+/*====================
+        functions
+====================*/
+const totalBudget = (array) => {
+    bankAccount = 0
+    for (let i = 0; i < arrBudget.length; i++){
+        bankAccount += parseInt(arrBudget[i].amount)
+    }
+}
 
 /*====================
         Routes
@@ -18,7 +30,9 @@ app.get("/",(req,res) => {
 
 app.get("/budgets",(req,res) => {
     // res.send("This is the - '/budgets GET' - path")
+    totalBudget(bankAccount)
     res.render("index",{
+        bankAccount: bankAccount,
         BudgetList: arrBudget
     })
 })
@@ -38,12 +52,14 @@ app.get("/budgets/:index",(req,res) => {
 app.post("/budgets",(req,res) => {
     // res.send("This is the - '/budgets POST' - path")
     let reqData = req.body
-    console.log(reqData)
     let arrTags = reqData.tags.split(", ")
+
     reqData.tags = arrTags
     arrBudget.push(reqData)
-    console.log(arrBudget)
+    
+    totalBudget(bankAccount)
     res.render("index",{
+        bankAccount: bankAccount,
         BudgetList: arrBudget
     })
 })
