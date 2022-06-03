de56aba9c50054e6e29b41480e7b17607d3c26a8
mkhan42 <michallekhan9@gmail.com>
2022-06-02 23:01:12 -0400

    created new route and post method and forms in new.liquid"

--- server.js
+++ server.js
@@ -2,20 +2,35 @@ const express = require('express')
 const app = require("liquid-express-views")(express())
 const budgets = require('./models/budget.js')
 
+
+const port = 3000
+
+app.use((req, res, next) => {
+    console.log('I run for all routes')
+    next()
+})
+
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(express.static('public'))
 
-const port = 3000
-
 app.get('/', (req, res) => {
     res.send('Hello')
+})
+
+app.post('/budgets', (req, res) => {
+    budgets.push(req.body)
+    res.redirect('/budgets')
 })
 
 app.get('/budgets/', (req, res) => {
     res.render('index.liquid', {
         allBudget: budgets
     })
+})
+
+app.get('/budgets/new', (req, res) => {
+    res.render('new.liquid')
 })
 
 app.get('/budgets/:index', (req, res) => {
