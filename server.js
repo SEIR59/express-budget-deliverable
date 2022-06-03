1e5aae929f54a60df6103bc9a207a9eb689d8add
peterxu35 <peter.px.xu@gmail.com>
2022-06-03 16:48:35 -0400

    server set up

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = require('liquid-express-views')(express())
+
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
