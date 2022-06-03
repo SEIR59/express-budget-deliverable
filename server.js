d6a20b4fa4d9e952bae4a82371d2118e9ac77f9e
idbic <dbicga@gmail.com>
2022-06-03 09:02:39 -0500

    commit before pull req

--- server.js
+++ server.js
@@ -2,21 +2,38 @@ const express = require('express')
 const budget = require('./models/budget')
 const app = require('liquid-express-views')(express())
 const port = 3000
+
+
 app.use(express.static('public'))
+// app.use(express.json());
+// app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
     res.send('Hello Badoogaler Alert')
 })
+ 
+app.use(express.urlencoded({extended: false}))
 
-app.get('/new/', (req, res) => {
-    res.render('new')
-})
 
 app.get('/index', (req, res) => {
     res.render('index', {
         myBudget: budget
     })
 })
+app.post('/index', (req, res) => {
+    
+    res.send(req.body)
+    
+    
+})
+
+
+app.get('/index/new/', (req, res) => {
+    res.render('new')
+})
+
+
+
 
 app.get('/index/:show', (req, res) => {
     res.render('show', {
