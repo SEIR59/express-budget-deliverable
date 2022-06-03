ea63bf94395035645f2773ffabe6932293971c0a
mkhan42 <michallekhan9@gmail.com>
2022-06-02 20:58:27 -0400

    added budgets/index riute

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = require("liquid-express-views")(express())
 const budget = require('./models/budget.js')
 
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
+
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello')
+})
+
+app.get('/budgets', (req, res) => {
+    res.send('Budgets')
 })
 
 
