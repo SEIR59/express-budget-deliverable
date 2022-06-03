c65d8d029f4ff6d35fb82246b0b8c80f217bba2a
christopher chapa <chapachristopher16@gmail.com>
2022-06-02 22:24:44 -0500

    logic for adding to bank account

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = require('liquid-express-views')(express())
 
 const Budget = require('./models/budget.js')
 const budget = require('./models/budget.js')
+let bankAccount = 0
+
 
 
 
@@ -16,14 +18,21 @@ app.get('/budget', (req ,res) => {
         'index',
         {
             allBudget: budget
-        }
+        },
+        bankAccount -= bankAccount
     )
 })
 
 app.post('/budget', (req, res) => {
     console.log(req.body)
     Budget.push(req.body)
+    budget.forEach(i => {
+        return Number(bankAccount += Number(i.amount))
+    })
+    
+    console.log(bankAccount)
     res.redirect('/budget')
+    bankAccount = null
 })
 
 app.get('/budget/new', (req, res) => {
@@ -36,7 +45,8 @@ app.get('/budget/:indexOfBudgetArray', (req, res) =>{
         'show',
         {
             budget: budget[req.params.indexOfBudgetArray]
-        }
+        },
+        
     )
 })
 
