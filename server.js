7ed3057f9cdcc38ec944097dbc067b646b571c18
earlhalasan <earl.halasan@gmail.com>
2022-06-02 22:19:28 -0400

    Filled out each link to display properties for each transaction.

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.listen(port, () => {
   console.log("app is running on port: ", port);
 });
 
+// Middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hi");
