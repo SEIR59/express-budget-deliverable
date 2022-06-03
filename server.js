c16e861827005d33740142e7e3ee47e76192ed7d
hnhtran <tran_eetas@yahoo.com>
2022-06-03 20:15:16 +1000

    set up post and middleware

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ app.listen(port, () => {
     console.log(`port 3000 listens`)
 })
 
+// test middleware
+app.use((req, res, next) => {
+    console.log(`I run for all route`)
+    next()
+})
+
+// tells express to try to match requests with files in the directory called 'public'
+app.use(express.static('public'))
+
+// allow us to access data that posting. Can be use add, edit, put, etc. i.e. allow developers use more than just get and post route
+app.use(express.urlencoded({extended: false}))
+
+app.post('/budgets', (req, res) => {
+    res.send(
+        'I am post'
+    )
+})
+
 app.get('/', (req, res) => {
     res.send(`Hello Tran!`)
 })
