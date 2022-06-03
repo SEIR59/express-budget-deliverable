5ed3c2660e416d82ba1f7794f8b8fc183db9ceb7
Mark Gerken <mgerken86@gmail.com>
2022-06-02 18:10:24 -0600

    total balance created with background changes if <0 or >1000

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const Budget = require('./models/budget')
 const app = require('liquid-express-views')(express())
 const port = 3000
 
+let bankAccount = 0
+const addExpenses = (arr) => {
+    bankAccount = 0
+    arr.forEach(item => {
+        bankAccount += Number(item.amount)
+    })
+    if (bankAccount < 0){
+        
+    }
+    return bankAccount
+}
+
 app.listen(port, () => {
     console.log('Listening on Budget Deliverable')
 })
@@ -19,12 +31,15 @@ app.use(express.static('public'))
 
 app.post('/budgets', (req, res) => {
     Budget.push(req.body)
+    addExpenses(Budget)
     res.redirect('/budgets')
 })
 
 app.get('/budgets', (req, res) => {
+    addExpenses(Budget)
     res.render('index', {
-        budget: Budget
+        budget: Budget,
+        bankAccount: bankAccount
     })
 })
 
