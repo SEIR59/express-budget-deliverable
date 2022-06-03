5674a19285093f288b4cf5e668a0edf4ac5805cc
Jason Lull <jlull7774@gmail.com>
2022-06-02 23:27:57 -0400

    IM SO SORRY FOR NOT COMMITING I WAS SO FOCUSED

--- server.js
+++ server.js
@@ -6,18 +6,37 @@ const port = 3000
 
 const budget = require("./model/budget.js")
 
+app.use(express.static("public"))
+
+app.use((req, res, next) => {
+  console.log("I run for all routes")
+  next()
+})
+
+app.use(express.urlencoded({ extended: false }))
+
 app.get("/budgets", (req, res) => {
-  res.render("index")
+  res.render("index", {
+    allBudgets: budget,
+  })
 })
 
-app.get("/budgets/:index", (req, res) => {
-  res.render("show")
-})
-
-app.get("/budgets/new", (req, res) => {
-  res.render("new")
+app.get('/budgets/new', (req, res) => {
+  res.render('new')
 })
 
 app.post("/budgets/new", (req, res) => {
-  res.redirect("budgets")
+  console.log(req.body)
+  budget.push(req.body)
+  res.redirect("/budgets")
+})
+
+app.get("/budgets/:index", (req, res) => {
+  res.render("show", {
+    budget: budget[req.params.index]
+  })
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
 })
