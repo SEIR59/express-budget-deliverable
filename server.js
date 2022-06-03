8b526def78f450fbd220e4239cb873460c982083
christopher chapa <chapachristopher16@gmail.com>
2022-06-02 19:33:22 -0500

    show.liquid link

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express')
+const Budget = require('./models/budget.js')
 const app = require('liquid-express-views')(express())
 
-app.listen(4000, () => {
-    console.log('listen to port 4000')
+const budget = require('./models/budget.js')
+
+app.use((req, res, next) =>{
+    console.log('I run for all routes')
+    next()
 })
 
 app.use(express.static('public'))
 
-app.get('/', (req ,res) => {
-    res.render('index')
+app.get('/budget', (req ,res) => {
+    res.render(
+        'index',
+        {
+            allBudget: budget
+        }
+    )
+})
+
+app.get('/budget/:indexOfBudgetArray', (req, res) =>{
+    res.render(
+        'show',
+        {
+            budget: budget[req.params.indexOfBudgetArray]
+        }
+    )
+})
+
+app.listen(4000, () => {
+    console.log('listen to port 4000')
 })
