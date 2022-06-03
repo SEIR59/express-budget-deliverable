27b3b6af15ad5f5c605e3eaad9e72c7349e3d8c8
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-06-02 21:33:09 -0400

    added show.liquid

--- server.js
+++ server.js
@@ -8,10 +8,18 @@ app.use(express.urlencoded({extended:false}));
 
 app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
 
-
-app.get('/budgtr',(req, res)=>{
+// index
+app.get('/budgets',(req, res)=>{
     res.render("index",{budgtr: Budget})
 })
+// show
+app.get('/budgets/:index',(req, res)=>{
+    res.render("show",{budgtr:Budget[req.params.index]})
+})
+
+// new
+
+// create
 
 app.listen(port,()=>{
     console.log("listening to port 3000")
