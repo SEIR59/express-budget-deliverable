bceb7e30261cdce25318d93f2f87b5f2dbc8272e
Hoskyfull <norberto.cardenales@gmail.com>
2022-06-04 13:21:27 -0600

    finished app

--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const budget = require('./models/budget');
 const app = require("liquid-express-views")(express());
+let bankAccount = 0
+const totalBudget = (array) => {
+    bankAccount = 0
+    array = budget
+    array.forEach(charge => {
+        bankAccount += parseInt(charge.amount)
+    })
+}
+
+budget.forEach(charge => {
+    bankAccount += charge.amount
+
+})
+
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.send('hello world!')
 })
 
-app.get('/budget', (req, res) => {
-    res.render('index', {
-        budget: budget,
-    })
-})
-app.get('/budget/:play', (req, res) => {
-    res.render('show', {
-        somethingToshow: budget[req.params.play]
+app.get('/budget/new', (req, res) => {
+    res.render('new', {
     })
 })
 
+app.get('/budget', (req, res) => {
+    totalBudget()
+    res.render('index', {
+        budget: budget,
+        bank: bankAccount
+    })
+})
+
+
+app.get('/budget/:play', (req, res) => {
+    // console.log(budget[req.params.play])
+    res.render('show', {
+        budget: budget[req.params.play]
+    })
+})
+
+app.post('/budget', (req, res) =>{
+    budget.push(req.body)
+    res.redirect('/budget')
+})
 
 
 app.listen(3000, () => {
