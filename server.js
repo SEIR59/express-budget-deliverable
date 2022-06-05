eb9a395169119722fb6fa34dbcf8a4a40f551339
Robert Romano <bromano1992@gmail.com>
2022-06-04 20:13:19 -0400

    Server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = require("liquid-express-views")(express());
+const budget = require("./models/budget.js");
+
+app.use(express.urlencoded({extended:false}));
+
+app.listen(3000, () => {
+    console.log("listening on port 3000!")
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
