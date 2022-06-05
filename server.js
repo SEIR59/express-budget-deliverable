f929053a4c3392edb1416d778b76b9fef384e689
peterxu35 <peter.px.xu@gmail.com>
2022-06-05 19:57:50 -0400

    create new.liquid

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = require('liquid-express-views')(express())
 const budget = require('./models/budget.js')
 
+app.use((req, res, next) => {
+    console.log('run routes');
+    next();
+});
+
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 
@@ -9,6 +14,10 @@ app.get('/', (req, res) => {
     res.render('index', {
         allItems: budget
     })
+})
+
+app.get('/new', (req, res) => {
+    res.render('new')
 })
 
 app.get('/:index', (req, res) => {
