a5e315c118bb93ce4ca725e55fc0219ad8d93153
victortan <vtan8090@gmail.com>
2022-06-05 13:00:42 -0400

    Added gets

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = require('liquid-express-views')(express())
 const port = 3000
 
+app.use(express.static(__dirname))
 
+app.get('/budgets', (req, res) => {
+    res.render('index')
+})
+
+app.get('budgets/new', (req, res) => {
+    res.render('new')
+})
+
+app.get('/budgets/:index', (req, res) => {
+    res.render('show', {
+        budget: budgets[req.params.index]
+    })
+})
 
 app.listen(port, () => {
     console.log(`Listening to port: ${port}`)
