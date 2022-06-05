a945ea63c21ef32cc1ec334032a6618ce5197369
mileslathan <mlathan.cn@gmail.com>
2022-06-05 19:18:56 -0400

    ** NEW ** Got the index.liquid to appear with proper css styling.

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = require('liquid-express-views')(express())
 const port = 3000
 
-app.get('/', (req, res) =>{
-    res.send("If you see me I work.")
-})
+// app.get('/', (req, res) =>{
+//     res.send("If you see me I work.")
+// })
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render(
+        'index',
+    )
+})
 
 app.listen(port, ()=> {
     console.log("Now Listening to Port 3000 :)")
