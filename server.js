d4436e093a12020fb510f47ca93fcc0bb9895bd5
Freightscape <jordan@freightscape.net>
2022-06-06 06:26:13 -0400

    HTML Page Renders

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = require("liquid-express-views")(express());
 const port = 3000;
 const budget = require("./budget");
 
+app.get("/", (req, res) => {
+  res.send("First HTML Page Renders Fine");
+});
+
 app.listen(port, () => {
   console.log("This Port is working Fine");
 });
