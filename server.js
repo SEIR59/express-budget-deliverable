d8f7707971d8d0a8c2454053e43cb87425f20e0e
Freightscape <jordan@freightscape.net>
2022-06-06 06:50:10 -0400

    Show & Index Pages Working

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ app.get("/", (req, res) => {
   res.send("First HTML Page Renders Fine");
 });
 
+app.get("/budgets", (req, res) => {
+  res.render("index");
+});
+
+app.get("/budgets/new", (req, res) => {
+  res.render("new");
+});
+
+app.get("/budgets/:index", (req, res) => {
+  res.render("show");
+});
+
 app.listen(port, () => {
   console.log("This Port is working Fine");
 });
