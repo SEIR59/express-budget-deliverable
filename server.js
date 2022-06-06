0baa09325309fb140619aa4bd0f18a65997452b0
annakoanna <anna.231991k@gmail.com>
2022-06-05 22:45:23 -0700

    main page of budget(index and server and show) and description page

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+
+const app = require("liquid-express-views")(express());
+
+const budget = require('./models/budget.js')
+
+app.use((req, res, next) => {
+    //  console.log('I run for all routes');
+    next();
+});
+//
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
+app.get('/budget', (req, res) => {
+    res.render('index', {
+        budget: budget,
+        
+    })
+})
+
+app.get('/budget/:key', (req, res) => {
+
+    res.render('show', {
+        budget: budget[req.params.key]
+    })
+
+})
+app.get('/new', (req, res) => {
+    console.log('sreing call ')
+    res.render('new')
+})
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
