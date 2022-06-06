adfffeba4d2a9ac02b521f3a6baf0a9c51b731de
bhenderson93 <brycehenderson1993@gmail.com>
2022-06-06 10:03:03 -0700

    Can add new items and display them properly on index page

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const app = require('liquid-express-views')(express())
 const budget = require('./models/budget.js')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}));
+
 
 app.get('/budgets', (req,res)=>{
 
     res.render('index', {
-        budgets: budget
+        budgets: budget,
     })
 })
 
-app.get('/budgets/new')
+app.get('/budgets/new', (req,res)=>{
+    res.render('new')
+})
 
 app.get('/budgets/:index', (req,res)=>{
     res.render('show',{
@@ -21,7 +25,11 @@ app.get('/budgets/:index', (req,res)=>{
     })
 })
 
-app.post('/budgets')
+app.post('/budgets', (req,res)=>{
+    req.body.tags = req.body.tags.split(',')
+    budget.push(req.body)
+    res.redirect('/budgets')
+})
 
 
 app.listen(3000, ()=>{
