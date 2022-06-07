792f50fadb4ffc22d5dae565a147f2a0ee725326
patwyzlic <patrickwyzlic@gmail.com>
2022-06-07 09:26:47 -0400

    Comments for server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+//Require these imports to run code
 const express = require('express');
 const budget = require('./Models/budget');
 const app = require("liquid-express-views")(express());
 const Budget = require('./Models/budget');
-
+//Sets port number
 const port = 3000;
 
+//General use for all routes
 app.use((req, res, next) => {
     console.log('I run for all routes');
     next();
 });
 
+//Middleware, for post method
 app.use(express.urlencoded({extended:false}));
 
+//Sets a total for the current bank account balance
 let bankAccount = 0;
 app.get('/budget', (req, res) =>{
     res.render('index', {
@@ -20,6 +24,7 @@ app.get('/budget', (req, res) =>{
     });
 });
 
+//Directs to the new budget submission area
 app.get('/budget/new', (req, res) => {
     res.render('new',
         {
@@ -27,11 +32,13 @@ app.get('/budget/new', (req, res) => {
         })
 });
 
+//Posts a new budget
 app.post('/budget/new', (req, res) => {
     Budget.push(req.body)
     res.redirect("/budget")
 });
 
+//Redirects to the specific budget, displays in a good format
 app.get('/budget/:index', (req, res) => {
     res.render('show',
         {
@@ -40,7 +47,7 @@ app.get('/budget/:index', (req, res) => {
 });
 
 
-
+//Listens to the 3000 port
 app.listen(port, () => {
     console.log('listening');
 });
