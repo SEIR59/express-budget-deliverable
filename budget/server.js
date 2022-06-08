13ac0f5bae3e0a97c11ae7057ff230139572a3d5
“GissellValdez” <Gissellvaldez@gmail.com>
2022-06-08 00:36:34 -0400

    created get route for new.liquid, added form to new.liquid

--- budget/server.js
+++ budget/server.js
@@ -4,6 +4,7 @@ const app = require('liquid-express-views')(express())
 const Budget = require('./models/budget.js')
 app.use(express.static('public'))
 const port = 3203
+app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}!!`)
@@ -11,6 +12,10 @@ app.listen(port, () => {
 
 app.get('/budgets', (req, res) => {
 	res.render('index',{ budgets: Budget})
+})
+
+app.get('/budgets/new', (req, res) => {
+	res.render('new')
 })
 
 app.get('/budgets/:indexOfBudgetData', (req, res) => {
