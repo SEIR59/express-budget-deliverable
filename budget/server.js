7fce15201542af2274d8ef7c22106d41ed945b77
“GissellValdez” <Gissellvaldez@gmail.com>
2022-06-08 00:52:42 -0400

    added post route

--- budget/server.js
+++ budget/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const req = require('express/lib/request')
 const res = require('express/lib/response')
 const app = require('liquid-express-views')(express())
 const Budget = require('./models/budget.js')
 app.use(express.static('public'))
 const port = 3203
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}!!`)
@@ -22,4 +24,11 @@ app.get('/budgets/:indexOfBudgetData', (req, res) => {
 	res.render('show', {
 		item: budgetData[req.params.indexOfBudgetData],
 	})
+})
+
+app.post('/budgets', (req, res) => {
+	//console.log(req.body)
+	Budget.push(req.body)
+	//console.log(Budget)
+	res.render('index', { budgets: Budget })
 })
