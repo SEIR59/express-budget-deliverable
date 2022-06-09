0c05c839f9f5231109949e91ac224731d6e4105b
Sal Dragone <104164651+sdragone01@users.noreply.github.com>
2022-06-09 16:32:02 -0400

    everything minus bank account

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express = require('express')
-const budgets = require ('./models/budget.js')
+const budgets = require('./models/budget.js')
 const app = require('liquid-express-views')(express())
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+const methodOverride = require("method-override")
+let sum = 0;
 
 
+budgets.forEach(element => {
+    sum += element.amount;
+  });
+  console.log(sum);
 
-app.get('/',(req,res) => {
+app.use(methodOverride("_method"))
+
+
+app.get('/', (req, res) => {
     res.send("hello")
 })
+
+//Delete Budget//
+app.delete('/budgets/:indexOfBudgetsArray', (req,res)=>{
+    budgets.splice(req.params.indexOfBudgetsArray, 1)
+    res.redirect('/budgets')
+})
+
+//New Budget//
+app.get('/budgets/new', (req, res) => {
+    res.render('new')
+})
+
+app.post('/budgets', (req, res) => {
+    budgets.push(req.body);
+
+    res.redirect('/budgets');
+});
 
 //Index Route//
 
@@ -16,22 +42,21 @@ app.get('/budgets/', (req, res) => {
     res.render(
         'index.liquid',
         {
-            allBudgets:budgets
+            allBudgets: budgets
         }
     )
 })
 
 //Show Route//
 
-app.get('/budgets/:indexOfBudgetsArray', (req, res)=>{
+app.get('/budgets/:indexOfBudgetsArray', (req, res) => {
     res.render('show', {
-        budgets: budgets[req.params.indexOfBudgetsArray] 
+        budgets: budgets[req.params.indexOfBudgetsArray]
     })
-}) 
+})
 
 
 
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("listning to port 3000")
 })
