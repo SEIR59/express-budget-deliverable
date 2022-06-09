6193517853beb24c1d99b1316e8fa1296986a79b
poojad2101 <davepooja2101@gmail.com>
2022-06-09 10:40:20 +0530

    setup files and gitignore

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
+const express = require('express');
 const budget = require('./models/budget.js')
+const app = require("liquid-express-views")(express())
 const port = 3000
 
 app.use(express.static('public'))
-//near the top, around other app.use() calls
+
+
 app.use(express.urlencoded({extended:false}));
 
-app.use((req, res, next) => {
-    console.log('I run for all routes');
-    next();
-});
+
+
+
+app.get('/' , (req, res) =>{
+    res.send('hello world')
+})
+
+
+app.get('/budget', (req, res) => {
+    res.render("index")
+})
 
 
 app.listen(port, () =>{
     console.log("listen to port 3000")
-})
-app.get('/' , (req, res) =>{
-    res.send('hello world')
 })
