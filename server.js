c05348332197aada8aa67f63a24aaac57ef7acaf
Edgie Manzo <104868823+emanzo14@users.noreply.github.com>
2022-06-09 14:51:02 -0400

    render index complete

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = require("liquid-express-views")(express())
+const Budget = require('./models/budget.js')
+
+
+let port = 3000
+
+app.listen(port, ()=>{
+    console.log("Using port: ", port)
+})
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
